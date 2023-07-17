fff552acca90e0d01622e8502f2091cfc4fa0540
jmartinez <jmartinez@shokworks.io>
2023-07-17 13:39:16 -0400

    (J) update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as socketIo from 'socket.io';
+import * as express from 'express';
+import * as http from 'http';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(3000);
-}
+
+  const httpServer = http.createServer(server);
+  const io = new socketIo.Server(httpServer);
+
+  io.on('connection', (client) => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+      console.log('Cliente desconectado');
+    });
+  });
+
+  await app.listen(3001);
+  }
+
 bootstrap();
